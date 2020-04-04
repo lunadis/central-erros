e8995adcfbc44ee745e03544020ab2e98dce3b6e
lucas-moraes <lucas.moraes88@yahoo.com>
2020-04-04 12:30:53 -0300

    + Controller - UsuárioController.cs

--- CentralErros.api/CentralErros/Controllers/UsuarioController.cs
+++ CentralErros.api/CentralErros/Controllers/UsuarioController.cs
@@ -1,17 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace CentralErros.Controllers
 {
-    [Route("api/usuario/[controller]")]
     [ApiController]
+    [Route("api/usuario/[controller]")]
+
     public class UsuarioController
     {
-        UsuarioController _usu;
 
-        //Verbos HTTP com as chamadas dos métodos do Repositório
+        private static List<Usuario> Usuarios = new List<Usuario>();
+
+        [HttpGet]
+        [Route("id:{id}")]
+        public List<Usuario> PuxarUsuarios()
+        {
+            return Usuarios;
+        }
+
+        [HttpPost]
+
+        [Route("CriarUsuarios")]
+        public List<Usuario> CadastrarUsuarios()
+        {
+            return Usuarios;
+        }
+
+        [Route("Login")]
+
+
+        [HttpPut]
+        [Route("EditarUsuarios")]
+        public List<Usuario> EditarUsuarios()
+        {
+            return Usuarios;
+        }
+        [HttpDelete]
+        [Route("DeletarUsuarios")]
+        public List<Usuario> DeletarUsuarios()
+        {
+            return Usuarios
+         }
+
+
     }
 }
