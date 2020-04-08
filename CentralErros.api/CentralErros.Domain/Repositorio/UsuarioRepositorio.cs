4c59be6d9ca0094004a02dbb14dba540a2c3b86d
Cordeiro96 <carlosmatheus1248@gmail.com>
2020-04-08 20:52:54 -0300

    Classe usuario Repositorio

--- CentralErros.api/CentralErros.Domain/Repositorio/UsuarioRepositorio.cs
+++ CentralErros.api/CentralErros.Domain/Repositorio/UsuarioRepositorio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CentralErros.Domain.Modelo;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,39 @@ namespace CentralErros.Domain.Repositorio
 {
     public class UsuarioRepositorio
     {
-        //Inteligência do Usuário
+        //private readonly Contexto _contexto;
+        public UsuarioRepositorio()
+        {
+            //_contexto = new Contexto();
+        }
+
+        public void Incluir(Usuario usuario)
+        {
+            //_contexto.Usuario.Add(usuario);
+            //_contexto.SaveChanges();
+        }
+
+        public void Alterar(Usuario usuario)
+        {
+            //_contexto.Usuario.Update(usuario);
+            //_contexto.SaveChanges();
+        }
+
+        public Usuario SelecionarPorId(int id)
+        {
+            //return _contexto.Usuario.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<Usuario> SelecionarTodos()
+        {
+            //return _contexto.Usuario.ToList();
+        }
+
+        public void Excluir(int id)
+        {
+            var usuario = SelecionarPorId(id);
+            //_contexto.Usuario.Remove(usuario);
+            //_contexto.SaveChanges();
+        }
     }
 }
